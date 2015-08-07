7735438ffa845834b7135f6770e7da6b2419647e
junquan_chen <junquan_chen@kingdee.com>
2015-08-07 13:02:54 +0800

    add dictapp

---
+++ DictApp/src/main/webapp/views/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$("#translate").click(function(){
+		var txt = $("#srctext").val();
+		if(txt.trim().length > 0) {
+			$.post("translate",
+				  {
+				    text:txt
+				  },
+				  function(result){
+					  var d = result.data;
+					  d = d.replace(/"/g, '');
+					  $("#show").text(d);
+				  }
+			 );
+		
+			
+		} else {
+			$("#show").text('请输入值');
+		}
+	})
+})
