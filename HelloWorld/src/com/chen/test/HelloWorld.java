09bca82155d70d07c3b724b2c57935a099f30bc4
junquan_chen <junquan_chen@kingdee.com>
2015-12-01 13:03:26 +0800

    修改

--- HelloWorld/src/com/chen/test/HelloWorld.java
+++ HelloWorld/src/com/chen/test/HelloWorld.java
@@ -1,8 +1,24 @@
 package com.chen.test;
+import org.jawin.COMException;
+import org.jawin.DispatchPtr;
+
+
 //我的第一个测试类
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello java");
+		DispatchPtr p2f = null;
+		try {
+			p2f = new DispatchPtr("Print2Flash4.Server");
+		} catch (Exception e) {
+			//如果4不存在，那么就取3
+			try {
+				p2f =  new DispatchPtr("Print2Flash3.Server");
+			} catch (COMException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		System.out.println("Hello");
 		
 	}
 }
